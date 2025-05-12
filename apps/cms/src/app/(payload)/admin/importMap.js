8732fab3cbac8fd68ed678428b4385d9699cc49a
jing <yangjing.yj56@gmail.com>
2025-05-12 16:49:08 +0800

    updated payload

--- apps/cms/src/app/(payload)/admin/importMap.js
+++ apps/cms/src/app/(payload)/admin/importMap.js
@@ -1,7 +1,9 @@
-import { CustomDashboard as CustomDashboard_7deecc071d1dcdb2ea0d650ba843e359 } from 'src/components/custom-add-on'
-import { PublishButton as PublishButton_7cb8e6b23124e6e8bd1b869695ac13cd } from 'src/components/publish'
+import { CustomDashboard as CustomDashboard_7deecc071d1dcdb2ea0d650ba843e359 } from '../../../components/custom-add-on'
+import { PublishButton as PublishButton_7cb8e6b23124e6e8bd1b869695ac13cd } from '../../../components/publish'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
   "/components/custom-add-on#CustomDashboard": CustomDashboard_7deecc071d1dcdb2ea0d650ba843e359,
-  "/components/publish#PublishButton": PublishButton_7cb8e6b23124e6e8bd1b869695ac13cd
+  "/components/publish#PublishButton": PublishButton_7cb8e6b23124e6e8bd1b869695ac13cd,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
