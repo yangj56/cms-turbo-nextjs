ce93d7b8dea40deabbdafeccdb0dceea65322028
jing <yangjing.yj56@gmail.com>
2025-06-28 11:07:23 +0800

    remove socials

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,10 +1,8 @@
 import { findHeros } from "@/actions/find-heros";
 import { findIntroduction } from "@/actions/find-introduction";
 import { findProductCategories } from "@/actions/find-product-categories";
-import { findSocials } from "@/actions/find-social";
 import { notFound } from "next/navigation";
 import type { JSX } from "react";
-import { BoxDisplay } from "../_components/box-display";
 import { Heros } from "../_components/heros";
 import { LandingIntroduction } from "../_components/landing-introduction";
 import { ProductCategories } from "../_components/product-categories";
@@ -13,14 +11,13 @@ export const dynamic = "force-static";
 export const revalidate = 60;
 
 export default async function HomePage(): Promise<JSX.Element> {
-  const [heros, socials, introduction, productCategories] = await Promise.all([
+  const [heros, introduction, productCategories] = await Promise.all([
     findHeros(),
-    findSocials(),
     findIntroduction(),
     findProductCategories(),
   ]);
 
-  if (!heros.length || !socials.length || !introduction || !productCategories.length) {
+  if (!heros.length || !introduction || !productCategories.length) {
     return notFound();
   }
 
@@ -49,7 +46,6 @@ export default async function HomePage(): Promise<JSX.Element> {
       <div className="container mb-20 mt-12 space-y-20 divide-y [&>*+*]:pt-12">
         <ProductCategories data={sortedProductCategories} />
         <LandingIntroduction data={introduction} />
-        <BoxDisplay label="Get Social" data={socials} />
       </div>
     </>
   );
